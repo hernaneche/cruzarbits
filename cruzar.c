66f928585999846bf326435783174b467729427e
Hernan Eche <hernan.eche@gmail.com>
2016-03-17 17:07:55 -0300

    first commit

---
+++ cruzar.c
@@ -0,0 +1,60 @@
+#define SIZE_TABLA 16
+
+int tabla[] = { 0,1,2,3,4,5,6,7,13,10,9,11,8,15,14,12 };  
+                
+//genera una tabla nueva para revertir el cruce
+void revertir( int *tabla, int *tablaInv, int size) {
+int ret = 0, num;
+  
+for( num = 0; num < size; num++ ){            
+        *(tablaInv+ *(tabla+num) )  = num;        
+  }
+
+for( num = 0; num < size; num++ ){            
+        printf (" %d", *(tablaInv+num));
+  }
+
+}
+
+int health( int *tabla , int size){
+	int times=0;
+	int i,j;
+for (i = 0; i < size; i++) {	
+    for (j = i + 1; j < size; j++) {
+        if ( *(tabla+i) == *(tabla+j) ||  *(tabla+i) >=size ) {
+            return 0;//error repeat or out of range 
+        }
+    }    
+ } 
+ return 1;//ok no repeat or out of range
+}
+
+//implementa cruce de bits segun tabla
+int cruzar( int in , int *tabla, int size) {	  
+  int ret = 0, num;  
+  for( num = 0; num < size; num++ ){      
+      if ( in & 0x01 ){
+          ret |= 1 << tabla[num];
+       }
+      in >>= 1;
+  }
+  return ret;
+}
+
+int main(){	
+
+int tablaInv[16];
+
+printf("health:[%d]", health(tabla, SIZE_TABLA) );
+
+revertir(tabla,tablaInv,SIZE_TABLA);
+
+	int nro=58433;
+	
+	int cruzado=cruzar(nro, tabla, SIZE_TABLA);
+	
+	int descruzado=cruzar(cruzado, tablaInv, SIZE_TABLA);
+
+	printf("\n%d->%d->%d",nro, cruzado, descruzado);
+
+}
